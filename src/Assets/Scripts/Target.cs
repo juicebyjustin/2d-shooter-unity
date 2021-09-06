4862aa06164458eb807b665f8dfea920f700dd3f
skettydog <2144123+juicebyjustin@users.noreply.github.com>
2021-09-05 21:30:29 -0500

    @34:45 Targets destroy themselves

--- src/Assets/Scripts/Target.cs
+++ src/Assets/Scripts/Target.cs
@@ -7,7 +7,7 @@ public class Target : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 2f);
     }
 
     // Update is called once per frame
